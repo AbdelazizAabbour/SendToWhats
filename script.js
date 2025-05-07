e2eb424b4a70a3367cb5507943a7451e27b4410c
ABDELAZIZ AABBOUR <azizabour77@gmail.com>
2025-05-07 17:20:32 +0100

    Update script.js
    update numbre phone

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById("whatsapp-form").addEventListener("submit", function(e) 
     var address = document.getElementById("address").value;
     
     
-    var number = "+212670955826";
+    var number = "+212620915747";
     
    
     var message = `Bonjour !\n\nNom : ${name}\nTéléphone : ${phone}\nProduit : ${product}\nAdresse : ${address}\n\nMerci pour votre commande.`;
